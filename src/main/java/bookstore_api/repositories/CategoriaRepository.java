11962665e8cda963f7506a8afb7a4969bdbbcd1f
jrfertonani <ademirjuniorfertonani@gmail.com>
2024-06-17 15:21:27 -0300

    Repository

---
+++ src/main/java/bookstore_api/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package bookstore_api.repositories;
+
+import bookstore_api.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
---
+++ src/main/java/bookstore_api/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package bookstore_api.repositories;
+
+import bookstore_api.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
