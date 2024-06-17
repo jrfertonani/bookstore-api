58e599a98e1c9c2535651c7ca6900d7421493834
jrfertonani <ademirjuniorfertonani@gmail.com>
2024-06-17 15:17:11 -0300

    Domain

---
+++ src/main/java/bookstore_api/domain/Categoria.java
@@ -0,0 +1,27 @@
+package bookstore_api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode
+@Entity
+public class Categoria implements Serializable {
+    private static final long seralVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+    private String nome;
+    private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private List<Livro> livros = new ArrayList<>();
+
+}
+
---
+++ src/main/java/bookstore_api/domain/Livro.java
@@ -0,0 +1,25 @@
+package bookstore_api.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Data
+@EqualsAndHashCode
+@Entity
+public class Livro implements Serializable {
+    private static final long seralVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Integer id;
+    private String titulo;
+    private String nome_autor;
+    private String texto;
+
+    @ManyToOne
+    @JoinColumn(name = "bookstore_id")
+    private Categoria categoria;
+}
